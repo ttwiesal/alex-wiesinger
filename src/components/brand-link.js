faad57041084819942d2ccecbb319df7d2b3c469
Alexander Wiesinger <alexander.wiesinger@grollmus.de>
2021-12-08 18:07:44 +0100

    use chakra ui to style buttons

--- src/components/brand-link.js
+++ src/components/brand-link.js
@@ -1,12 +1,15 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { Link, Button } from "@chakra-ui/react"
 
-const BrandLink = ({ iconName, text, link }) => (
-  <div>
-    <FontAwesomeIcon icon={["fab", iconName]} />
-    <a href={link}>{text}</a>
-  </div>
+const BrandLink = ({ iconName, text, link, color }) => (
+  <Link href={link} isExternal>
+    <Button colorScheme={color} width="100%">
+      <FontAwesomeIcon icon={["fab", iconName]} />
+      {text}
+    </Button>
+  </Link>
 )
 
 BrandLink.propTypes = {
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,32 +10,39 @@ import {
   faLinkedin,
   faXing,
 } from "@fortawesome/free-brands-svg-icons"
+import { VStack } from "@chakra-ui/react"
 
 library.add(fab, faGithub, faInstagram, faXing, faLinkedin)
 
 const IndexPage = () => (
   <Layout>
     <h1>Hi people</h1>
-    <BrandLink
-      iconName="github"
-      text="Github"
-      link="https://github.com/ttwiesal"
-    />
-    <BrandLink
-      iconName="xing"
-      text="Xing"
-      link="https://www.xing.com/profile/Alexander_Wiesinger4/cv"
-    />
-    <BrandLink
-      iconName="linkedin"
-      text="LinkedIn"
-      link="https://www.linkedin.com/in/alexander-wiesinger-589b4399/"
-    />
-    <BrandLink
-      iconName="instagram"
-      text="Instagram"
-      link="https://www.instagram.com/ttwiesal/"
-    />
+    <VStack align="stretch">
+      <BrandLink
+        iconName="github"
+        text="Github"
+        link="https://github.com/ttwiesal"
+        color="black"
+      />
+      <BrandLink
+        iconName="xing"
+        text="Xing"
+        link="https://www.xing.com/profile/Alexander_Wiesinger4/cv"
+        color="green"
+      />
+      <BrandLink
+        iconName="linkedin"
+        text="LinkedIn"
+        link="https://www.linkedin.com/in/alexander-wiesinger-589b4399/"
+        color="blue"
+      />
+      <BrandLink
+        iconName="instagram"
+        text="Instagram"
+        link="https://www.instagram.com/ttwiesal/"
+        color="pink"
+      />
+    </VStack>
   </Layout>
 )
 
